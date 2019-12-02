d16c6f5dd548b8cb545e5846f5a8fdd8e5a492f5
rnd <rnd@DESKTOP-HHI4566>
2019-12-02 10:02:12 +0900

    오류처리

--- src/main/java/com/pcn/manager/controller/ErrorPageController.java
+++ src/main/java/com/pcn/manager/controller/ErrorPageController.java
@@ -1,11 +1,29 @@
 package com.pcn.manager.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * @class ErrorController
@@ -21,11 +39,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * - 클래스의 업무내용에 대해 기술...
 */
 
-//@Controller
+@Controller
 public class ErrorPageController implements ErrorController {
+	
+	Logger logger = LogManager.getLogger(getClass());
 
-    private static final String ERROR_PATH = "/error/error";
-
+    private static final String ERROR_PATH = "/error";
     /**
      * @fn
      * @remark
@@ -38,7 +57,49 @@ public class ErrorPageController implements ErrorController {
     }
 
     @RequestMapping(ERROR_PATH)
-    public String handleError(HttpServletRequest request, Model model) {
-        return "error404";
+    public String handleError(HttpServletRequest req, HttpServletResponse res,@RequestHeader HttpHeaders headers, Exception e) throws Exception {
+    	
+    	this.errorLogWrite(e);
+    	
+    	String str_path = "";
+        Object statusCode = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        
+        UrlPathHelper urlPathHelper = new UrlPathHelper(); 
+        String originalURL = urlPathHelper.getOriginatingRequestUri(req);
+        str_path = originalURL;
+        
+        if("404".equals(statusCode.toString())) {
+        	str_path = "error404";
+        }else {
+        	str_path = "error500";
+        	throw e;
+        }
+        return str_path;
     }
+    
+	private void errorLogWrite(Exception e) {
+		
+		ByteArrayOutputStream byteOut = null;
+		String error = "";
+		
+		try {
+			byteOut = new ByteArrayOutputStream();
+			e.printStackTrace(new PrintStream(byteOut));
+			byteOut.flush();
+			error = new String(byteOut.toByteArray());
+			byteOut.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} finally {
+			if(byteOut != null)
+				try {
+					byteOut.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		}
+		logger.error(error);
+	}
 }
